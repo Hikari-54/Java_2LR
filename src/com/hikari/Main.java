85b72bcd2db9085f3ce8bb0d70274ce91f3bbe91
Hikari-54 <climberte@yandex.ru>
2022-02-17 21:00:38 +0300

    Разбираюсь с наследованием

---
+++ src/com/hikari/Main.java
@@ -0,0 +1,17 @@
+package com.hikari;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Point3d test3d_1 = new Point3d(1, 2, 3);
+        test3d_1.setX(15);
+        test3d_1.setY(117);
+        test3d_1.setZ(269);
+        System.out.println(test3d_1.getX());
+        System.out.println(test3d_1.getY());
+        System.out.println(test3d_1.getZ());
+
+
+    }
+}
